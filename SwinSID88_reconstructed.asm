3e8332d559a3fb29cc3934f3835ab27f318366e9
dmantione <daniel.mantione@freepascal.org>
2022-08-05 11:04:38 +0200

    Update SwinSID88_reconstructed.asm

--- SwinSID88_reconstructed.asm
+++ SwinSID88_reconstructed.asm
@@ -8,6 +8,8 @@
 ; Lazy Jones fix developed by Máté "CodeKiller" Sebök
 ;
 ; Source code reconstruction by Daniël Mantione
+;
+; Set your editor tab size to 4.
 ;############################################################################
 
 
